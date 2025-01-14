a6d9d04c4b16c35940181a4262827f8bde7927d1
Raghav jha <46911401+RAGHAVJHA01@users.noreply.github.com>
2025-01-14 20:17:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask import render_template
 
-# Create an instance of the Flask class
 app = Flask(__name__)
 
-# Define a route
-@app.route('/')
-def home():
-    return "Welcome to the Dummy Flask App!"
 
-@app.route('/api/data')
-def get_data():
-    data = {
-        "name": "Flask App",
-        "version": "1.0",
-        "description": "This is a dummy API endpoint."
-    }
-    return jsonify(data)
-
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
